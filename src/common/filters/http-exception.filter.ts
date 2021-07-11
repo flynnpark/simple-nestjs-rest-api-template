3f07ff307927b9234271cb8f6c82a6c89e40b453
Flynn <wphestiraid@gmail.com>
2021-07-11 15:51:14 +0900

    HttpExceptionFilter 구현

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      success: false,
+      path: request.path,
+      message: exception.message,
+      result: null,
+    });
+  }
+}
